9b7b21efa288e671dd730de6f0b9cf51b0b5b1d2
Samkiroko <kirokodev@gmail.com>
2021-06-08 23:36:54 +0300

    add form input data

--- .history/src/components/NewExpense/ExpenseForm_20210608232026.js
+++ .history/src/components/NewExpense/ExpenseForm_20210608232026.js
@@ -17,18 +17,8 @@ export const ExpenseForm = () => {
     setEnteredDate(event.target.value)
   }
 
-  const submitHandler = (event) => {
+  const submitHandler = (evnt) => {
     event.preventDefault()
-
-    const expenseData = {
-      title: enteredTitle,
-      amount: enteredAmount,
-      date: new Date(enteredDate),
-    }
-    console.log(expenseData)
-    setEnteredTitle('')
-    setEnteredAmount('')
-    setEnteredDate('')
   }
 
   return (
@@ -36,15 +26,15 @@ export const ExpenseForm = () => {
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' value={enteredTitle} onChange={titleChangeHandler} />
+          <input type='text' onChange={titleChangeHandler} />
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
-          <input type='number' min='0.01' step='0.01' value={enteredAmount} onChange={amountChangeHandler} />
+          <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler} />
         </div>
         <div className='new-expense__control'>
           <label>Date</label>
-          <input type='date' min='2019-01-01' max='2022-12-31' value={enteredDate} onChange={dateChangeHandler} />
+          <input type='date' min='2019-01-01' max='2022-12-31' onChange={dateChangeHandler} />
         </div>
       </div>
       <div className='new-expense__actions'>
